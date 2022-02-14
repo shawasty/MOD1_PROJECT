19aa612d31cb32add10132bf747b9d0c71c64bd1
shawasty <shawasty@gmail.com>
2022-02-14 13:59:39 -0500

    created a perfect shuffle of 52 cards array

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 // create variables for the different suits and their values
-const suits =[ '♠', '♥', '♦','♣']   //obtained from google ('HTML suit symbols')
+const suits =[ '♠', '♥', '♦','♣'];  //obtained from google ('HTML suit symbols')
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-class Deck {
+export default class Deck {
     // creat a class to take care of the deck, 
     //constructor takes an arguement of cards because the cars could be anything , either a full
     //52 cards or in my case , 26 for each 
-    constructor(cards) {
+    constructor(cards = freshDeck()) {
         this.cards = cards
+    };
+    shuffle(){
+        let lenCards = this.cards.length;
+        //simple way to suffle
+        for (let i =lenCards -1; i > 0; i--){
+            const newIndex = Math.floor(Math.random() * (i + 1));
+            //the following assigns new indexes from the newly gained index to the original
+            const oldIndex = this.cards[i];
+            this.cards[newIndex] = this.cards[i];
+            this.cards[i] = oldIndex;
 
+
+        }
+        // this.cards.sort((a,b)=>Math.random()- .5)
     }
-}
+};
 // create a class for cards ( since a card can vary from A - K) with an aguements of suit, value
 class Card {
     constructor(suit , value){
@@ -18,4 +32,12 @@ class Card {
 
     }
 
-}
+};
+function freshDeck(){
+    return suits.flatMap(suit =>{
+        //flatMap fxn turns 4 different arrays of 13 into one formated array
+       return values.map(value =>{
+            return new Card(suit, value)
+        })
+    })
+};
---
+++ script.js
@@ -0,0 +1,7 @@
+import Deck from './main.js';
+
+const deck = new Deck();
+
+deck.shuffle()
+
+console.log(deck.cards);
