2806d7ba675f6eb046689cef6e5105f6ba0cd219
shawasty <shawasty@gmail.com>
2022-02-14 21:54:11 -0500

    added a function that resets counter before start and also a counter function

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 import Deck from './main.js';
+// const computerCaSlot = document.querySelector('.computer-card');
+const playerCaSlot = document.querySelector('.player-card');
+const compCarSlot = document.querySelector('.computer-card');
+const computerDeckEle = document.querySelector('.computer-deck');
+const playerDeckEle = document.querySelector('.player-deck');
+const text = document.querySelector('.text');
 
 
 
-const compCarSlot = document.querySelector('.computer-card-slot');
 
 let playerDeck, compuDeck;
 
@@ -18,8 +23,21 @@ const midDeck = Math.ceil(deck.lenCards/2);
 playerDeck = new Deck(deck.cards.slice(0, midDeck));
 compuDeck = new Deck(deck.cards.slice(midDeck , deck.lenCards))
 
-console.log(playerDeck)
-console.log(compuDeck)
+// console.log(playerDeck)
+// console.log(compuDeck)
+startReset()
 
+}
+function startReset(){
+    compCarSlot.innerHTML ='';
+    playerCaSlot.innerHTML ='';
+    text.innerText ='';
+
+    updateDeckCount()
+}
+
+function updateDeckCount(){
+    computerDeckEle.innerText = compuDeck.lenCards;
+    playerDeckEle.innerText = playerDeck.lenCards;
 }
 // compCarSlot.appendChild(deck.cards[0].getHTML())
