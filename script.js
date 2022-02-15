6f43e7edde49848222b8816b2510b81b1e96ac06
shawasty <shawasty@gmail.com>
2022-02-15 11:08:08 -0500

    added an update of player and computer card and lose or win situations

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 import Deck from './main.js';
+
+//because of A,J,Q,K in the cards , convert them to their 
+//numerical values to help check the winner 
+
+const numCardValue ={
+    "2" : 2,
+    "3" : 3,
+    "4" : 4,
+    "5" : 5,
+    "6" : 6,
+    "7" : 7,
+    "8" : 8,
+    "9" : 9,
+    "10": 10,
+    "J" : 11,
+    "Q" : 12,
+    "K" : 13,
+    "A" : 14
+
+}
 // const computerCaSlot = document.querySelector('.computer-card');
 const playerCaSlot = document.querySelector('.player-card-slot');
 const compCarSlot = document.querySelector('.computer-card-slot');
@@ -10,7 +30,8 @@ const text = document.querySelector('.text');
 
 
 let playerDeck, compuDeck
-let inSession = false  //set default value to false
+let inSession = false;  //set default value to false
+let stopGame = false;
 
 document.addEventListener('click', ()=>{
     if (inSession){
@@ -34,6 +55,7 @@ function startGame(){
     playerDeck = new Deck(deck.cards.slice(0, midDeck));
     compuDeck = new Deck(deck.cards.slice(midDeck , deck.lenCards));
     inSession = false;
+    stopGame = false;
 
 
 
@@ -50,6 +72,7 @@ function startReset(){
     text.innerText ='';
 
     updateDeckCount()
+
 }
 
 function flipcard(){
@@ -60,11 +83,53 @@ function flipcard(){
     const computerCard = compuDeck.pop();
 
     playerCaSlot.appendChild(playerCard.getHTML());
-    compCarSlot.appendChild(computerCard.getHTML())
+    compCarSlot.appendChild(computerCard.getHTML());
+
+    updateDeckCount()
+// check with the roundWinner function's condition, if playerCard won
+    if(roundWinner(playerCard, computerCard)){
+        text.innerText = 'Win'
+        playerDeck.push(playerCard);
+        playerDeck.push(computerCard);
+        //add sound
+        //add emojie
+    }   //  check if computerCard won
+    else  if(!roundWinner(playerCard, computerCard)){
+        text.innerText = 'Lose'
+        compuDeck.push(playerCard);
+        compuDeck.push(computerCard);
+        // add sound
+        //add emojie
+    } else{
+        text.innerText = 'Draw'
+        playerDeck.push(playerCard);
+        compuDeck.push(computerCard);
+        // add sound
+    }
+
+    if (gameOver(playerDeck)){
+        text.innerText = 'You lose !!!';
+        stopGame = true
+    }else if(gameOver(compuDeck)){
+        text.innerText = 'You lose !!!';
+        stopGame = true
+    }
+    
+
 }
 
 function updateDeckCount(){
+
     computerDeckEle.innerText = compuDeck.lenCards;
     playerDeckEle.innerText = playerDeck.lenCards;
+}
+
+function roundWinner (card1, card2){
+    return numCardValue[card1.value]>numCardValue[card2.value]
+    
+}
+
+function gameOver(deck){
+    return deck.lenCards === 0;
 }
 // compCarSlot.appendChild(deck.cards[0].getHTML())
